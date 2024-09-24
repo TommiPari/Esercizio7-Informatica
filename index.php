a8870cbb4dc466b5e99d0724803a578c9fea4715
Tommaso Parigi <tommaso.parigi06@gmail.com>
2024-09-24 12:44:23 +0200

    Page completed

--- index.php
+++ index.php
@@ -7,7 +7,29 @@
 </head>
 <body>
     <?php
-        
+        $x = rand(1,30);
+        $y = rand(1,30);
+        echo "$x $y";
+        if ($x > $y) {
+            $max = $x;
+            $frase = "MAGGIORE";
+        } else {
+            $max = $y;
+            if ($x == $y) {
+                $frase = "UGUALE";
+            } else {
+                $frase = "MINORE";
+            }
+        }
+        echo "<h1> $frase </h1>";
+        if ($max < 10) {
+            echo "<p> Num $max minore di 10 </p>";
+        } else if ($max > 20) {
+            echo "<p> Num $max maggiore di 20 </p>";
+        } else {
+            echo "<p> Num $max compreso fra 10 e 20 </p>";
+        }
+
     ?>
 </body>
 </html>
